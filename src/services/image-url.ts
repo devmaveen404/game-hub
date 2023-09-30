cf681ca860298acee00d0d5161f50ced6964dfe3
Maveen <ogunrindexxx@gmail.com>
2023-09-30 06:23:23 -0700

    handling games without image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,9 @@
+import noImage from '../assets/rockstar.ico'
 // 9, Optimize images
 const getCroppedImageUrl = (url: string) => {
+// 20, no Image cases
+if (!url) return noImage 
+
     const index = url.indexOf('media/') + 'media/'.length
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
 };
