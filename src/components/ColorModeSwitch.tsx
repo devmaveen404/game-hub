f10d387e76d7a89d75d119d69016dfdd1fb40156
Maveen <ogunrindexxx@gmail.com>
2023-09-30 06:51:50 -0700

    build search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -7,12 +7,12 @@ const ColorModeSwitch = () => {
 
   return (
     <HStack>
-      <Switch
+      <Switch 
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       ></Switch>
-      <Text>Dark Mode</Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import Searchinput from "./Searchinput";
 
 const Navbar = () => {
   return (
     // to render items horizontally
     <HStack justifyContent="space-between" padding={3}>
       <Image src={logo} borderRadius={15} boxSize={79}></Image>
+      <Searchinput/>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
