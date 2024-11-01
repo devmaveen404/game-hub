c923acdd804464289aff6402e843ee16e26b5275
maven <devmaveen333@yahoo.com>
2024-11-01 09:35:46 +0000

    fix error page

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import history from 'connect-history-api-fallback'
-import type { Plugin } from 'vite'
 
-// Custom plugin to integrate connect-history-api-fallback
-function historyFallback(): Plugin {
-  return {
-    name: 'single-page-app-fallback',
-    configureServer(server) {
-      server.middlewares.use(
-        history({
-          // Options (optional): specify which paths to ignore, if any
-          // e.g., `rewrites: [{ from: /\/api/, to: '/index.html' }]`
-        })
-      )
-    },
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), historyFallback()],
+  plugins: [react()],
 })
